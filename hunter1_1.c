cd6417872d9728249ad412828d46e8237673e15e
VivinJoshua <34916945+VivinJoshua@users.noreply.github.com>
2019-06-11 10:54:30 +0530

    Create hunter1_1.c

---
+++ hunter1_1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int n,arr[100],ar[100]={0},flag=0;
+scanf("%d",&n);
+for(int i=0;i<n;i++)
+{
+  scanf("%d",&arr[i]);
+  ar[arr[i]]++;
+ }
+ for(int i=0;i<100;i++)
+ {
+ if(ar[i]>1)
+  {
+  printf("%d ",i);
+  flag=1;
+  }
+  }
+  if(flag=0)
+    printf("unique");
+    }
