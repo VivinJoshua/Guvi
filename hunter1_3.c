9ec1ecbc6437590f85d3a369a603b3828a083b85
VivinJoshua <34916945+VivinJoshua@users.noreply.github.com>
2019-06-11 10:41:38 +0530

    Update hunter1_3.c

--- hunter1_3.c
+++ hunter1_3.c
@@ -1,12 +1,21 @@
 #include<stdio.h>
 int main()
 {
-int n,arr[100];
+int n,arr[100],index=0,arr1[100];
 scanf("%d",&n);
 for(int i=0;i<n;i++)
   {
   scanf("%d",&arr[i]);
-  if(arr[i]==i
-    printf("%d",i);
+  if(arr[i]==i)
+     {
+     arr1[index++]=i;
+     }
    }
+     if(index==0)
+      printf("-1");
+    else
+     {
+       for(int i=0;i<index;i++)
+          printf("%d ",arr1[i]);
+     }
    }
