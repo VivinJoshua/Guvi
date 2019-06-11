160b72a046eaafd035e37e398c761f35d8b9df76
VivinJoshua <34916945+VivinJoshua@users.noreply.github.com>
2019-06-11 09:32:22 +0530

    Update set1_3.c

--- set1_3.c
+++ set1_3.c
@@ -1,10 +1,16 @@
 #include<stdio.h>
+#include<stdlib.h>
 int main()
 {
 char a;
 scanf("%c",&a);
-if(a=='a' || a=='e' || a=='i' || a=='o' || a=='u')
-  printf("Vowel");
-else
-  printf("Consonant");
+  if(isalpha(a))
+  {
+    if(a=='a' || a=='e' || a=='i' || a=='o' || a=='u')
+      printf("Vowel");
+    else
+      printf("Consonant");
+  }
+  else
+      printf("Invalid");
   }
