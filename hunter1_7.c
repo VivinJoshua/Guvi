287cd9519f8e0a83f1c58e871bb60acc9445565d
VivinJoshua <34916945+VivinJoshua@users.noreply.github.com>
2019-06-11 11:52:10 +0530

    Create hunter1_7.c

---
+++ hunter1_7.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int n,arr[100];
+scanf("%d",&n);
+for(int i=0;i<n;i++)
+  scanf("%d",&arr[i]);
+for(int i=0;i<n;i++)
+{
+  if(i%2==1)
+    {
+      if(arr[i]%2==0)
+        printf("%d ",arr[i]);
+    }
+   else if(i%2==0)
+    {
+      if(arr[i]%2==1)
+        printf("%d ",arr[i]);
+     }
+  }
+  }
