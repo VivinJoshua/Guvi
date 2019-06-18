c17a852b6c65b3b22472665d0daf6ad712dac301
VivinJoshua <34916945+VivinJoshua@users.noreply.github.com>
2019-06-18 20:15:44 +0530

    Update player1_1.c

--- player1_1.c
+++ player1_1.c
@@ -1,11 +1,18 @@
+//string reverse
 #include<stdio.h>
 #include<string.h>
 int main()
 {
-char name[100];
+char name[100],temp;
 scanf("%s",name);
 int len=strlen(name);
-for(int i=len-1;i>=0;i--)
-  printf("%c",name[i]);
+len-=1;
+for(int i=0;i<=len/2;i++)
+{
+    temp=name[i];
+    name[i]=name[len-i];
+    name[len-i]=temp;
+}
+  printf("%s",name);
 return 0;
 }
