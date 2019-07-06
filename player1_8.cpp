0acf4044439333653f59562a96eaddc1c4dd035b
VivinJoshua <34916945+VivinJoshua@users.noreply.github.com>
2019-07-06 08:21:56 -0700

    Update player1_8.cpp

--- player1_8.cpp
+++ player1_8.cpp
@@ -4,12 +4,22 @@ int main()
 {
   char str[1000];
   cin.getline(str,sizeof(str));
-  str[0]=toupper(str[0]);
+ 
   for(int i=0;str[i];i++)
   {
-  if(str[i]==' ' && str[i+1])
-    str[i+1]=toupper(str[i+1]);
+  if(str[i]==' ' && str[i+1]!='\0')
+    {
+        // str[i+1]=toupper(str[i+1]);
+        if(!(str[i+1]>=65 && str[i+1]<=90))
+        // cout<<"i";
+        str[i+1]-=32;
+    }
+    else
+    if((str[i+1]>=65 && str[i+1]<=90))
+        // cout<<"i";
+        str[i+1]+=32;
   }
+   str[0]=toupper(str[0]);
   cout<<str;
 //   cout<<"\n"<<toupper(str[0]);
   return 0;
